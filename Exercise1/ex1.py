d78acdf58713ab2390a04a2a8c89b08adb154aff
codenstuff <jesse.goebel.18@gmail.com>
2021-03-08 18:35:14 -0600

    Add exercise 1

---
+++ Exercise1/ex1.py
@@ -0,0 +1,17 @@
+# Python 2 syntax shown below for reference
+""" print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.' """
+
+# The translated Python 3 code
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
