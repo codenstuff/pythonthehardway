1cc4979302786375263799e3d58a4eb49ed1b508
codenstuff <jesse.goebel.18@gmail.com>
2021-03-08 20:21:23 -0600

    Add exercise 2

---
+++ Exercise2/studydrills.py
@@ -0,0 +1,6 @@
+# 1. Find out if you were right about what the # character does and make sure you know
+# what it's called (octothorpe or pound character).
+print('The # symbol is called an octothorpe or pound character.')
+
+# 2. Take your ex2.py file and review each line going backward. Start at the last line, and check each word in reverse against what you should have typed.
+print('I have verified the file contents by going backwards through it and confirmed the text is correct.')
