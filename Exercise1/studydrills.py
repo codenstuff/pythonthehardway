b21eeb6f0ea1f53be693528005dafb21a73d7e88
codenstuff <jesse.goebel.18@gmail.com>
2021-03-08 19:51:15 -0600

    Add exercise 1 study drills

---
+++ Exercise1/studydrills.py
@@ -0,0 +1,17 @@
+from platform import python_version
+# Study drills for Exercise 1. 
+#Rather than expanding on the original script, I am going to write new statements with the same result.
+
+# 1. Make your script print another line.
+print('In Python 3, print has changed from a statement to a function.')
+print('In Python 2, you might write "print \'Hello!\'"')
+print('In Python 3, you would write "print(\'Hello!\')"')
+print('Since I am using Python', python_version(),'I use the function syntax.')
+
+# 2. Make your script print only one of the lines.
+print('The purpose of these drills are to become confident in using print()')
+
+# 3. Put a "#" (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+print('Another part of this exercise is to become familiar with code comments.')
+print('In Python, single-line comments are indicated by the "#" character.')
+print('Alternatively, a multi-line comment can be made using a set of \"\"\" characters.')
